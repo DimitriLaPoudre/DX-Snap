a52a1df18731f0807802f54afc88d3aa89bce694
frenchkiwi <raphael.launay@epitech.eu>
2025-08-26 00:15:15 +0200

    feat: Added client base

---
+++ client/lib/menu/home_page.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/material.dart';
+
+import '../network/tcp_client.dart';
+
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  final TcpClient _tcpClient = TcpClient();
+
+  @override
+  void initState() {
+    super.initState();
+    connectToServer();
+  }
+
+  void connectToServer() {
+    try {
+      _tcpClient.connect('127.0.0.1', 8080);
+    } catch (e) {
+      debugPrint("Error connecting to server: $e");
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('DX Snap Home Page'),
+      ),
+      body: const Center(
+        child: Text('Welcome to DX Snap!'),
+      ),
+    );
+  }
+}
